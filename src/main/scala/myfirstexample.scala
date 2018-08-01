f92f3c2eae2cce47b38b7ec33c139a6881fab19f
24santoshr <24santoshr@gmail.com>
2018-08-01 16:12:25 +0200

    travis CI

---
+++ src/main/scala/myfirstexample.scala
@@ -0,0 +1,28 @@
+
+import java.util.{Date, Locale}
+import java.text.DateFormat._
+
+object myfirstexample {
+
+  def main(args: Array[String]): Unit = {
+
+
+
+
+    val a=3
+    val b=4
+    val add= (a+b)
+
+    //val now =new Date
+    //val df= getDateInstance(LONG, Locale.CHINA)
+
+
+    println("The current time in China is")
+
+    println("hello world")
+
+    //val tm=getTimeInstance();
+    println(add)
+  }
+}
+
